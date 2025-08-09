91986fab26700e11a5f8c2e33a3be740fab29145
Sherwin S. Abion <Sherwin.Abion@CONVERGEICT.COM>
2025-08-09 12:13:28 +0800

    recommeded page and category-card fix:layout overflow

--- src/app/pages/recommended/recommended-landing/recommended-landing.component.ts
+++ src/app/pages/recommended/recommended-landing/recommended-landing.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 
+import { CategoryCardComponent } from '../../../shared/components/category-card/category-card.component';
+import { mocksCategories } from '../../../shared/mocks/mocks-category';
+
 @Component({
   selector: 'app-recommended-landing',
   standalone: true,
-  imports: [],
+  imports: [CategoryCardComponent],
   templateUrl: './recommended-landing.component.html',
-  styleUrl: './recommended-landing.component.scss'
+  styleUrl: './recommended-landing.component.scss',
 })
 export class RecommendedLandingComponent {
-
+  categories = mocksCategories;
 }
