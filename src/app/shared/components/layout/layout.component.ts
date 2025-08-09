791fc2664947ed15c8144bb38b2f3cf59d55853a
Sherwin S. Abion <Sherwin.Abion@CONVERGEICT.COM>
2025-08-09 12:34:59 +0800

    integrate footer

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
+import { FooterComponent } from '../footer/footer.component';
 import { NavigationComponent } from '../navigation/navigation.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [NavigationComponent],
+  imports: [NavigationComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
