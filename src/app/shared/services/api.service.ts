cfc4926f09ebb384613e50d1833edf5aa69f9f2f
Sherwin S. Abion <Sherwin.Abion@CONVERGEICT.COM>
2025-08-21 19:58:02 +0800

    product filters and product category

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -18,15 +18,22 @@ export class ApiService {
     return this.#http.get<ICategory[]>(`${this.#baseUrl}/api/category/`);
   }
 
-  getSubCategories(category_id: number): Observable<ISubCategory[]> {
+  getSubCategories(category_id?: number): Observable<ISubCategory[]> {
     return this.#http.get<ISubCategory[]>(
       `${this.#baseUrl}/api/subcategory/?category_id=${category_id}`,
     );
   }
 
-  getProducts(category_id: number): Observable<IProduct[]> {
-    return this.#http.get<IProduct[]>(
-      `${this.#baseUrl}/api/product/?category_id=${category_id}`,
-    );
+  getProducts(
+    categoryId?: number,
+    subcategoryId?: number,
+  ): Observable<IProduct[]> {
+    const params: { category_id?: number; subcategory_id?: number } = {};
+    categoryId ? (params.category_id = categoryId) : undefined;
+    subcategoryId ? (params.subcategory_id = subcategoryId) : undefined;
+
+    return this.#http.get<IProduct[]>(`${this.#baseUrl}/api/product/`, {
+      params,
+    });
   }
 }
